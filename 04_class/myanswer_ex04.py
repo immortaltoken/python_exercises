23534f522a482c9b201d91e050919c45e4cf8d04
TakechiKentaro <potatomasher2nd@gmail.com>
2021-10-06 13:47:22 +0900

    mohankaitoumo naositayo

--- 04_class/myanswer_ex04.py
+++ 04_class/myanswer_ex04.py
@@ -5,14 +5,19 @@ import unittest
 class Bot():
 
     def __init__(self, owner_name):
+        """
+        WRITE YOUR CODE HERE
+        """
         self.owner_name = owner_name
 
     def reply(self, call):
+        """
+        WRITE YOUR CODE HERE
+        """
         if self.owner_name in call:
             return "Hello my Boss!"
         else:
             return "Hello"
-
 
 class TestBot(unittest.TestCase):
 
